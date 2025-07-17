cd1dbd404fe422af9b309d901bb4fbf360829546
Taha6360 <syedtaha8989@gmail.com>
2025-07-17 11:56:04 +0530

    Day 1 updated

---
+++ DAY 1/Band Generator .py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+user = input("What's the name of the city u grew up in?\n")
+pet = input("what's your pet's name?\n")
+print("Your band name could be " + user+""+pet)
